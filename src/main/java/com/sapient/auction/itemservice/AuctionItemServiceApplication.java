7015f606becc0a1ab00e493c8a674805f1e97cd3
Vishwamitra Hegde <vishwamitra.hegde@sapient.com>
2016-12-01 17:02:23 +0530

    Initial commit

---
+++ src/main/java/com/sapient/auction/itemservice/AuctionItemServiceApplication.java
@@ -0,0 +1,14 @@
+package com.sapient.auction.itemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class AuctionItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionItemServiceApplication.class, args);
+	}
+}
